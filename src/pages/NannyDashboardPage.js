5769f9dabde44423d30c9452333c06c1e931baea
sdi2100258 <sdi2100258@di.uoa.gr>
2025-01-14 22:55:50 +0200

    new

--- src/pages/NannyDashboardPage.js
+++ src/pages/NannyDashboardPage.js
@@ -6,18 +6,19 @@ import AuthContext from '../contexts/AuthContext';
 import './NannyDashboardPage.css';
 import Navbar from '../components/Navbar';
 
+
 function NannyDashboardPage() {
   const [nanny, setNanny] = useState(null);
-  const [error, setError] = useState('');
-   const {currentUser} = useContext(AuthContext);
+    const [error, setError] = useState('');
+    const {currentUser} = useContext(AuthContext);
   const [ads, setAds] = useState([]);
-    const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
 
-   useEffect(() => {
+  useEffect(() => {
       console.log('NannyDashboardPage: useEffect triggered');
       fetchNanny();
       fetchAds();
-    }, []);
+  }, []);
 
     const fetchAds = async () => {
         console.log('NannyDashboardPage: fetchAds started');
@@ -30,7 +31,8 @@ function NannyDashboardPage() {
         catch(error){
            console.error('NannyDashboardPage: Error fetching ads', error);
            setError('Failed to load ads. Please try again later.');
-        } finally {
+        }
+      finally {
             setLoading(false);
         }
     };
@@ -38,65 +40,65 @@ function NannyDashboardPage() {
 
     const fetchNanny = async () => {
         console.log('NannyDashboardPage: fetchNanny started');
-         try {
-             console.log('NannyDashboardPage: currentUser.uid: ', currentUser.uid);
-             const querySnapshot = await getDocs(query(collection(db, 'nannies'), where('userId', '==', currentUser.uid)))
-             const fetchedNannies = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+        try {
+            console.log('NannyDashboardPage: currentUser.uid: ', currentUser.uid);
+            const querySnapshot = await getDocs(query(collection(db, 'nannies'), where('userId', '==', currentUser.uid)));
+              const fetchedNannies = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
             if (fetchedNannies.length > 0) {
               setNanny(fetchedNannies[0]);
               console.log('NannyDashboardPage: fetchNanny success', fetchedNannies[0]);
-          } else {
-                 console.warn('NannyDashboardPage: fetchNanny no nanny found');
-          }
-        }
-       catch (error) {
-            console.error('NannyDashboardPage: Error fetching nanny:', error);
-            setError('Failed to load nanny profile. Please try again later.');
+           } else {
+              console.warn('NannyDashboardPage: fetchNanny no nanny found');
          }
+      }
+      catch (error) {
+          console.error('NannyDashboardPage: Error fetching nanny:', error);
+           setError('Failed to load nanny profile. Please try again later.');
+        }
       finally {
-            setLoading(false);
+          setLoading(false);
       }
     }
 
 
-    if (loading) {
-      return <p>Loading nanny dashboard...</p>;
+   if (loading) {
+        return <p>Loading nanny dashboard...</p>;
     }
-    if (error){
-      return <p className="error-message">{error}</p>;
-    }
-   if(!nanny) {
-      return <p>No nanny data found</p>;
-     }
+   if (error){
+       return <p className="error-message">{error}</p>;
+   }
+    if(!nanny) {
+       return <p>No nanny data found</p>;
+   }
 
-    return (
+   return (
        <>
         <Navbar />
     <div className="nanny-dashboard-page">
       <h2>Καλώς Ήρθες {nanny.name}</h2>
-        <div className="dashboard-buttons">
-        <Link to='/edit-nanny-profile' className='edit-profile-button'>Επεξεργασία Προφίλ</Link>
-         <Link to='/upload-legal-doc' className='upload-legal-button'>Upload Legal Docs</Link>
-        <Link to='/create-ad' className='create-ad-button'>Δημιουργία Αγγελίας</Link>
-         <Link to='/nanny/manage-ads' className='manage-ads-button'>Manage Ads</Link>
-        <Link to='/manage-appointments' className='manage-appointments-button'>Διαχείριση Ραντεβού</Link>
-          <Link to='/schedule' className='schedule-button'>Manage Schedule</Link>
-        </div>
+         <div className="dashboard-buttons">
+           <Link to='/edit-nanny-profile' className='edit-profile-button'>Επεξεργασία Προφίλ</Link>
+             <Link to='/upload-legal-doc' className='upload-legal-button'>Upload Legal Docs</Link>
+             <Link to='/create-ad' className='create-ad-button'>Δημιουργία Αγγελίας</Link>
+           <Link to='/nanny/manage-ads' className='manage-ads-button'>Manage Ads</Link>
+         <Link to='/manage-appointments' className='manage-appointments-button'>Διαχείριση Ραντεβού</Link>
+         <Link to='/schedule' className='schedule-button'>Manage Schedule</Link>
+          </div>
 
         <div className="ads-section">
-        <h3>Οι Αγγελίες Μου</h3>
-            {ads.length > 0 ? (
-              ads.map(ad => (
-                  <div key={ad.id} className="ad-card">
-                      <p><strong>Τίτλος:</strong>{ad.name}</p>
-                     <p><strong>Διαθεσιμότητα:</strong>{ad.availability}</p>
-                    <p><strong>Απασχόληση:</strong> {ad.employment}</p>
-                 </div>
+            <h3>Οι Αγγελίες Μου</h3>
+              {ads.length > 0 ? (
+                  ads.map(ad => (
+                    <div key={ad.id} className="ad-card">
+                       <p><strong>Τίτλος:</strong>{ad.name}</p>
+                    <p><strong>Διαθεσιμότητα:</strong>{ad.availability}</p>
+                   <p><strong>Απασχόληση:</strong> {ad.employment}</p>
+                  </div>
                ))
-             ) : (
-               <p>Δεν υπάρχουν αγγελίες.</p>
-            )}
-       </div>
+                ) : (
+                   <p>Δεν υπάρχουν αγγελίες.</p>
+               )}
+          </div>
     </div>
        </>
  );
--- src/pages/RegistrationPage.js
+++ src/pages/RegistrationPage.js
@@ -2,87 +2,106 @@ import React, { useState, useContext } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import AuthContext from '../contexts/AuthContext';
 import './RegistrationPage.css';
+import { doc, setDoc, collection, addDoc } from 'firebase/firestore';
+import { db } from '../firebase';
+
 
 function RegistrationPage() {
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-  const [role, setRole] = useState('parent'); // Default role parent
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [role, setRole] = useState('parent');
     const [error, setError] = useState(null);
   const { register } = useContext(AuthContext);
   const navigate = useNavigate();
 
+
   const handleRegister = async (e) => {
-    e.preventDefault();
+     e.preventDefault();
     if (password !== confirmPassword) {
-        setError("Οι κωδικοί δεν ταιριάζουν.");
+      setError("Οι κωδικοί δεν ταιριάζουν.");
       return;
     }
-      const { user, error } = await register(email, password, firstName, lastName, role);
-    if (user) {
-        if(role === 'parent'){
-            navigate('/search-nannies');
-        }
-        else {
-            navigate('/nanny-dashboard');
-        }
-    } else if (error) {
-      setError(error);
-    }
+        const { user, error } = await register(email, password, firstName, lastName, role);
+        if(user){
+           if (role === 'nanny') {
+               try {
+                  const nanniesCollection = collection(db, 'nannies');
+                   await addDoc(nanniesCollection, {
+                       userId: user.uid,
+                       name: `${firstName} ${lastName}`,
+                       email: email,
+                   });
+                   const userRef = doc(db, 'users', user.uid);
+                   await setDoc(userRef, { firstName: firstName, lastName: lastName, email: email, role: role});
+               } catch (err) {
+                 console.error("Error creating nanny profile:", err);
+                 setError('Failed to create nanny profile, please try again later.');
+                return;
+             }
+             navigate('/nanny-dashboard');
+          }
+           else {
+               const userRef = doc(db, 'users', user.uid);
+               await setDoc(userRef, { firstName: firstName, lastName: lastName, email: email, role: role});
+              navigate('/search-nannies');
+            }
+         } else if (error) {
+            setError(error);
+         }
   };
- const handleTaxisnetRegister = () => {
-      //Redirect to Taxisnet registration
-      window.location.href = 'https://login.gsis.gr/mylogin/login.jsp?bmctx=1DB55AB50C08F2B418903DE4EB7466AD47038BC455E39B9EA82B1EB28CE52BC6&contextType=external&username=string&password=secure_string&challenge_url=https%3A%2F%2Flogin.gsis.gr%2Fmylogin%2Flogin.jsp&ssoCookie=disablehttponly&request_id=-5842705815979373440&authn_try_count=0&locale=en_US&resource_url=https%253A%252F%252Fwww1.gsis.gr%252Ftaxisnet%252Fmytaxisnet%252Fprotected%252Fauthorizations.htm'; // Replace with your actual TaxisNet URL
-  }
 
-    const handleRoleChange = (e) => {
-        setRole(e.target.value);
-    }
-
+  const handleTaxisnetRegister = () => {
+     //Redirect to Taxisnet registration
+     window.location.href = 'https://login.gsis.gr/mylogin/login.jsp?bmctx=1DB55AB50C08F2B418903DE4EB7466AD47038BC455E39B9EA82B1EB28CE52BC6&contextType=external&username=string&password=secure_string&challenge_url=https%3A%2F%2Flogin.gsis.gr%2Fmylogin%2Flogin.jsp&ssoCookie=disablehttponly&request_id=-5842705815979373440&authn_try_count=0&locale=en_US&resource_url=https%253A%252F%252Fwww1.gsis.gr%252Ftaxisnet%252Fmytaxisnet%252Fprotected%252Fauthorizations.htm'; // Replace with your actual TaxisNet URL
+   };
+   const handleRoleChange = (e) => {
+      setRole(e.target.value);
+    };
   return (
     <div className="registration-page">
       <div className="registration-form">
         <h2>Σελίδα Εγγραφής</h2>
-          {error && <p className="error-message">{error}</p>}
+        {error && <p className="error-message">{error}</p>}
         <form onSubmit={handleRegister}>
           <div className="form-group">
             <label htmlFor="firstName">Όνομα</label>
             <input type="text" id="firstName" value={firstName} onChange={(e) => setFirstName(e.target.value)} required />
           </div>
-            <div className="form-group">
-                <label htmlFor="lastName">Επώνυμο</label>
-                <input type="text" id="lastName" value={lastName} onChange={(e) => setLastName(e.target.value)} required />
-            </div>
           <div className="form-group">
-            <label htmlFor="email">Email</label>
-            <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+            <label htmlFor="lastName">Επώνυμο</label>
+            <input type="text" id="lastName" value={lastName} onChange={(e) => setLastName(e.target.value)} required />
           </div>
           <div className="form-group">
+           <label htmlFor="email">Email</label>
+           <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+         </div>
+          <div className="form-group">
             <label htmlFor="password">Κωδικός</label>
-            <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+             <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
           </div>
             <div className="form-group">
               <label htmlFor="confirmPassword">Επιβεβαίωση Κωδικού</label>
-              <input type="password" id="confirmPassword" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} required />
-            </div>
+             <input type="password" id="confirmPassword" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} required />
+          </div>
            <div className="form-group">
-              <label htmlFor='role'>Είμαι</label>
+            <label htmlFor='role'>Είμαι</label>
                <select id='role' value={role} onChange={handleRoleChange}>
-                  <option value='parent'>Γονέας</option>
-                   <option value='nanny'>Νταντά/Επιμελητής</option>
-               </select>
+                   <option value='parent'>Γονέας</option>
+                 <option value='nanny'>Νταντά/Επιμελητής</option>
+              </select>
             </div>
-            <button type="submit" className="register-button">Ολοκλήρωση Εγγραφής</button>
-            <button type="button" onClick={handleTaxisnetRegister} className="taxisnet-register-button">Εγγραφή με TaxisNet</button>
+          <button type="submit" className="register-button">Ολοκλήρωση Εγγραφής</button>
+        <button type="button" onClick={handleTaxisnetRegister} className="taxisnet-register-button">Εγγραφή με TaxisNet</button>
             <div className="register-footer">
               <Link to="/login">Έχετε Λογαριασμό; Είσοδος</Link>
-          </div>
+           </div>
         </form>
       </div>
-    </div>
-  );
+   </div>
+ );
 }
 
 export default RegistrationPage;
