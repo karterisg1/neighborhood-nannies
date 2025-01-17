b8c172c2744bd1c1cb2374bf60374f0acf87deb8
karterisg <sdi2100258@di.uoa.gr>
2025-01-17 14:26:44 +0200

    new

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,7 +1,7 @@
-export function formatDate(date) {
-  return new Date(date).toLocaleDateString('el-GR', { day: '2-digit', month: '2-digit', year: 'numeric' });
-}
-export function generateChatId(userId1, userId2) {
+ export function formatDate(date) {
+    return new Date(date).toLocaleDateString('el-GR', { day: '2-digit', month: '2-digit', year: 'numeric' });
+  }
+ export function generateChatId(userId1, userId2) {
     const sortedIds = [userId1, userId2].sort();
-  return `${sortedIds[0]}-${sortedIds[1]}`;
-}
+    return `${sortedIds[0]}-${sortedIds[1]}`;
+  }
